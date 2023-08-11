63630b879bed8f68629e5a231442fc63c129ab91
M Naufa Dzulfiqar <muhammadnaufa007@gmail.com>
2023-08-11 23:00:59 +0700

    first commit

---
+++ database/seeders/SettingTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('setting')->insert([
+            'id_setting' => 1,
+            'nama_perusahaan' => 'Toko Sepatu',
+            'alamat' => 'Jl. Pojok Tengah No 108 Cimahi',
+            'telepon' => '089635440315',
+            'tipe_nota' => 1, // kecil
+            'diskon' => 5,
+            'path_logo' => '/img/logo.png',
+            'path_kartu_member' => '/img/member.png',
+        ]);
+    }
+}
